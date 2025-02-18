acb5c460f8a397a115961e6419645c0b20695441
ymehili <youssefmehili@gmail.com>
2025-02-18 11:58:44 +0100

    add 4 elementary component

---
+++ src/ElementaryComponents/OrComponent.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** Untitled (Workspace)
+** File description:
+** OrComponent
+*/
+
+#ifndef ORCOMPONENT_HPP_
+#define ORCOMPONENT_HPP_
+
+#include "../AComponent.hpp"
+
+namespace nts {
+    class OrComponent : public AComponent {
+        public:
+            OrComponent() : AComponent("or", 3) {}
+            ~OrComponent() = default;
+
+            Tristate compute(std::size_t pin = 3) {
+                if (pin != 3)
+                    return Tristate::Undefined;
+                Tristate a = _links[0].first ? _links[0].first->compute(_links[0].second) : Tristate::Undefined;
+                Tristate b = _links[1].first ? _links[1].first->compute(_links[1].second) : Tristate::Undefined;
+                if (a == Tristate::True || b == Tristate::True)
+                    return Tristate::True;
+                if (a == Tristate::Undefined && b == Tristate::Undefined)
+                    return Tristate::Undefined;
+                return Tristate::False;
+            }
+    };
+}
+
+#endif /* !ORCOMPONENT_HPP_ */
---
+++ src/ElementaryComponents/XorComponent.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** Untitled (Workspace)
+** File description:
+** XorComponent
+*/
+
+#ifndef XORCOMPONENT_HPP_
+#define XORCOMPONENT_HPP_
+
+#include "../AComponent.hpp"
+
+namespace nts {
+    class XorComponent : public AComponent {
+        public:
+            XorComponent() : AComponent("xor", 3) {}
+            ~XorComponent() = default;
+
+            Tristate compute(std::size_t pin = 3) {
+                if (pin != 3)
+                    return Tristate::Undefined;
+                Tristate a = _links[0].first ? _links[0].first->compute(_links[0].second) : Tristate::Undefined;
+                Tristate b = _links[1].first ? _links[1].first->compute(_links[1].second) : Tristate::Undefined;
+                if (a == Tristate::True || b == Tristate::True)
+                    return Tristate::True;
+                if (a == Tristate::Undefined && b == Tristate::Undefined)
+                    return Tristate::Undefined;
+                return Tristate::False;
+            }
+    };
+}
+
+#endif /* !XORCOMPONENT_HPP_ */
