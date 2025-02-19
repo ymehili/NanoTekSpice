6e022ef796364fcdb6644dd7ac6c53b26b4cfb2b
Spencer <pay.spencer@gmail.com>
2025-02-19 11:10:37 +0100

    [fix] trailing space

--- src/IComponent.hpp
+++ src/IComponent.hpp
@@ -24,7 +24,7 @@ namespace nts
         virtual ~IComponent() = default;
         virtual void simulate(std::size_t tick) = 0;
         virtual nts::Tristate compute(std::size_t pin) = 0;
-        virtual void setLink(std::size_t pin, nts ::IComponent &other, std::size_t otherPin) = 0;
+        virtual void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin) = 0;
     };
 }
 
