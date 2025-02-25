8fee56a47795b7c18b0158c14314224a9d2d4bae
ymehili <youssefmehili@gmail.com>
2025-02-25 08:30:57 +0100

    fix xor

--- src/ElementaryComponents/XorComponent.hpp
+++ src/ElementaryComponents/XorComponent.hpp
@@ -10,23 +10,30 @@
 
 #include "../AComponent.hpp"
 
-namespace nts {
-    class XorComponent : public AComponent {
-        public:
-            XorComponent() : AComponent(3) {}
-            ~XorComponent() = default;
+namespace nts
+{
+    class XorComponent : public AComponent
+    {
+    public:
+        XorComponent() : AComponent(3) {}
+        ~XorComponent() = default;
 
-            Tristate compute(std::size_t pin = 3) {
-                if (pin != 3)
-                    return Tristate::Undefined;
-                Tristate a = _links[0].first ? _links[0].first->compute(_links[0].second) : Tristate::Undefined;
-                Tristate b = _links[1].first ? _links[1].first->compute(_links[1].second) : Tristate::Undefined;
-                if (a == Tristate::True || b == Tristate::True)
-                    return Tristate::True;
-                if (a == Tristate::Undefined && b == Tristate::Undefined)
-                    return Tristate::Undefined;
-                return Tristate::False;
-            }
+        Tristate compute(std::size_t pin = 3)
+        {
+            if (pin != 3)
+                return Tristate::Undefined;
+            Tristate a = _links[0].first ? _links[0].first->compute(_links[0].second) : Tristate::Undefined;
+            Tristate b = _links[1].first ? _links[1].first->compute(_links[1].second) : Tristate::Undefined;
+
+            if (a == Tristate::Undefined || b == Tristate::Undefined)
+                return Tristate::Undefined;
+
+            if ((a == Tristate::True && b == Tristate::False) ||
+                (a == Tristate::False && b == Tristate::True))
+                return Tristate::True;
+
+            return Tristate::False;
+        }
     };
 }
 
