6ea289a4f60d89d78d2978e8a8ba50f02903adc1
ymehili <youssefmehili@gmail.com>
2025-03-08 12:38:33 +0100

    fix shift

--- src/AdvancedComponents/C4094_shift.cpp
+++ src/AdvancedComponents/C4094_shift.cpp
@@ -16,40 +16,43 @@ namespace nts
         Tristate strobe = _links[1 - 1].first ? _links[1 - 1].first->compute(_links[1 - 1].second) : Tristate::Undefined;
         Tristate enable = _links[15 - 1].first ? _links[15 - 1].first->compute(_links[15 - 1].second) : Tristate::Undefined;
 
-        if (_prevclock != Tristate::True && clock == Tristate::True) {
+        if (_prevclock != Tristate::True && clock == Tristate::True)
+        {
             for (int i = 7; i > 0; i--)
                 _shiftreg[i] = _shiftreg[i - 1];
             _shiftreg[0] = data;
+
+            if (strobe == Tristate::True)
+            {
+                for (int i = 0; i < 8; i++)
+                    _storedreg[i] = _shiftreg[i];
+            }
         }
 
         _prevclock = clock;
 
-        if (strobe == Tristate::True) {
-            for (int i = 0; i < 8; i++)
-                _storedreg[i] = _shiftreg[i];
-        }
-
-        switch (pin) {
-            case 4:
-                return enable == Tristate::True ? _storedreg[0] : Tristate::Undefined;
-            case 5:
-                return enable == Tristate::True ? _storedreg[1] : Tristate::Undefined;
-            case 6:
-                return enable == Tristate::True ? _storedreg[2] : Tristate::Undefined;
-            case 7:
-                return enable == Tristate::True ? _storedreg[3] : Tristate::Undefined;
-            case 14:
-                return enable == Tristate::True ? _storedreg[4] : Tristate::Undefined;
-            case 13:
-                return enable == Tristate::True ? _storedreg[5] : Tristate::Undefined;
-            case 12:
-                return enable == Tristate::True ? _storedreg[6] : Tristate::Undefined;
-            case 11:
-                return enable == Tristate::True ? _storedreg[7] : Tristate::Undefined;
-            case 9:
-                return _shiftreg[7];
-            case 10:
-                return enable;
+        switch (pin)
+        {
+        case 4:
+            return enable == Tristate::True ? _storedreg[0] : Tristate::Undefined;
+        case 5:
+            return enable == Tristate::True ? _storedreg[1] : Tristate::Undefined;
+        case 6:
+            return enable == Tristate::True ? _storedreg[2] : Tristate::Undefined;
+        case 7:
+            return enable == Tristate::True ? _storedreg[3] : Tristate::Undefined;
+        case 14:
+            return enable == Tristate::True ? _storedreg[4] : Tristate::Undefined;
+        case 13:
+            return enable == Tristate::True ? _storedreg[5] : Tristate::Undefined;
+        case 12:
+            return enable == Tristate::True ? _storedreg[6] : Tristate::Undefined;
+        case 11:
+            return enable == Tristate::True ? _storedreg[7] : Tristate::Undefined;
+        case 9:
+            return enable == Tristate::True ? _shiftreg[7] : Tristate::Undefined;
+        case 10:
+            return enable;
         }
         return Tristate::Undefined;
     }
